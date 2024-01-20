dae64beb3e788a82a111b057c88a5dfd314a2418
DavidADavisSAP <139809109+DavidADavisSAP@users.noreply.github.com>
2024-01-20 11:41:20 -0500

    Delete src/environments/firebase-config.ts

--- src/environments/firebase-config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCbzKG_RnS4V5QDg2FoxUEOF39bZpzH77w",
-  authDomain: "my-personal-kitchen.firebaseapp.com",
-  databaseURL: "https://my-personal-kitchen.firebaseio.com",
-  projectId: "my-personal-kitchen",
-  storageBucket: "my-personal-kitchen.appspot.com",
-  messagingSenderId: "45285634202",
-  appId: "1:45285634202:web:e82d159118561426422f23"
-};
